59500d50a34f1355c8dbe2cdcce5a9e0acc4b8f9
Andrii-forknu <andrii.velychko@knu.ua>
2025-03-30 19:17:01 +0300

    Created Hand

--- Clock.py
+++ Clock.py
@@ -6,7 +6,7 @@ from typing import Protocol
 
 
 class Digit:
-    def __init__(self, value: int, position: tuple[float, float], size: float = 1.0):
+    def init(self, value: int, position: tuple[float, float], size: float = 1.0):
         self.value = value
         self.position = position
         self.size = size
@@ -23,7 +23,7 @@ class Digit:
 
 
 class ClockFace:
-    def __init__(self, radius: float, center: tuple[float, float] = (0, 0)):
+    def init(self, radius: float, center: tuple[float, float] = (0, 0)):
         self.radius = radius
         self.center = center
         self.digits: list[Digit] = []
@@ -31,3 +31,52 @@ class ClockFace:
         self._turtle.hideturtle()
         self._turtle.speed(0)
 
+    def setup(self) -> None:
+        import math
+        for hour in range(1, 13):
+            angle = math.radians(30 * (3 - hour))
+            x = self.center[0] + (self.radius * 0.85) * math.cos(angle)
+            y = self.center[1] + (self.radius * 0.85) * math.sin(angle)
+            digit = Digit(hour, (x, y), size=1.2)
+            self.digits.append(digit)
+
+    def draw(self) -> None:
+        self._turtle.penup()
+        self._turtle.goto(self.center[0], self.center[1] - self.radius)
+        self._turtle.pendown()
+        self._turtle.circle(self.radius)
+        for _ in range(12):
+            self._turtle.penup()
+            self._turtle.goto(self.center[0], self.center[1])
+            self._turtle.setheading(_ * 30)
+            self._turtle.forward(self.radius * 0.9)
+            self._turtle.pendown()
+            self._turtle.forward(self.radius * 0.1)
+        for digit in self.digits:
+            digit.draw()
+
+
+class Hand:
+    def init(self, length: float, width: float, color: str, center: tuple[float, float] = (0, 0)):
+        self.length = length
+        self.width = width
+        self.color = color
+        self.center = center
+        self.angle = 0
+        self._turtle = turtle.Turtle()
+        self._turtle.hideturtle()
+        self._turtle.speed(0)
+
+    def draw(self) -> None:
+        self._turtle.clear()
+        self._turtle.penup()
+        self._turtle.goto(self.center)
+        self._turtle.pensize(self.width)
+        self._turtle.color(self.color)
+        self._turtle.setheading(90 - self.angle)
+        self._turtle.pendown()
+        self._turtle.forward(self.length)
+
+    def update(self, angle: float) -> None:
+        self.angle = angle
+        self.draw()
